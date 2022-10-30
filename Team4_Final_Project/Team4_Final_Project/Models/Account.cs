41164f916ad55cd8ac2e96379a33e819e4ff4110
graysonmerritt <graysonmerritt@gmail.com>
2022-10-30 14:24:11 -0500

    started models

---
+++ Team4_Final_Project/Team4_Final_Project/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+public enum AccountType { "Checking", "Savings"}
+namespace Team4_Final_Project.Models
+{
+    public class Account
+    {
+
+        [Display(Name = "First Name")]
+        public AccountType Type { get; set; }
+    }
+}
---
+++ Team4_Final_Project/Team4_Final_Project/Models/AppUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Team_4_Final_Project.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Display(Name = "First Name")]
+        public String FirstName { get; set; }
+        [Display(Name = "Middle Initital")]
+        public String MiddleInitial { get; set; }
+
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+
+        [Display(Name = "Street")]
+        public String Street { get; set; }
+        [Display(Name = "City")]
+        public String City { get; set; }
+        [Display(Name = "State")]
+        public String State { get; set; }
+        [Display(Name = "Zipcode")]
+        public String Zipcode { get; set; }
+        [Display(Name = "Phone Number")]
+        public String PhoneNumber { get; set; }
+        [Display(Name = "Birthday")]
+        public DateTime Birthday { get; set; }
+
+        [Display(Name = "Is Active")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "Accounts")]
+        public Account Accounts { get; set; }
+
+    }
+}
+
