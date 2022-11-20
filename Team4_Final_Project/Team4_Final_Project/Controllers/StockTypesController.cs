da62cf06c3244e9e4d32e74917960a25bd7ff1c8
graysonmerritt <graysonmerritt@gmail.com>
2022-11-20 16:53:52 -0600

    quick fix

--- Team4_Final_Project/Team4_Final_Project/Controllers/StockTypesController.cs
+++ Team4_Final_Project/Team4_Final_Project/Controllers/StockTypesController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +12,7 @@ using Team4_Final_Project.Models;
 
 namespace Team4_Final_Project.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class StockTypesController : Controller
     {
         private readonly AppDbContext _context;
